ad202387a9b5929d4435a77d7322733d0bc64169
combsgabe <combsgabe@gmail.com>
2015-09-09 22:26:38 -0600

    update index, js, css

--- scripts/etchJs.js
+++ scripts/etchJs.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
-	for(i=1;i<15;i++){
-		$('#container').append('<div class="sketchBox"></div>');
-	};
+	populateGrid(16,950);
+	$('#size').click(function(){
+		var rows = prompt("Enter # of rows","");
+		var width = prompt("Enter width of grid","");
+		populateGrid(rows, width);
+	});
+	
+	$(document).click(function(){
+		$('.sketchBox').hover(function(){
+			$(this).css("background-color","blue");
+		});		
+	});
+	
+	$('#clear').click(function(){
+		$('.sketchBox').css("background-color","black");
+	});
 });
+
+function populateGrid(numBoxes, gridWidth){
+	$('#grid').width(gridWidth);	
+	for(x=1;x<=numBoxes;x++){
+		for(i=1;i<=numBoxes;i++){
+			$('#grid').append('<div class="sketchBox"></div>');
+		};
+	};
+	var boxWidth = gridWidth/(parseInt(numBoxes)+parseInt(1));
+	$('.sketchBox').css('width',boxWidth);
+	$('.sketchBox').css('height',boxWidth);
+};
